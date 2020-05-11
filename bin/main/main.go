247093113d274f9857abf911cbd21c753202caba
pefish <pefish@qq.com>
2020-05-11 16:10:44 +0800

    add

--- bin/main/main.go
+++ bin/main/main.go
@@ -3,50 +3,48 @@ package main
 import (
 	"_template_/constant"
 	"_template_/route"
-	"fmt"
 	"github.com/pefish/go-application"
 	"github.com/pefish/go-config"
-	go_core "github.com/pefish/go-core"
 	"github.com/pefish/go-core/driver/logger"
 	global_api_strategy "github.com/pefish/go-core/global-api-strategy"
+	"github.com/pefish/go-core/service"
 	"github.com/pefish/go-logger"
 	"github.com/pefish/go-mysql"
-	"os"
-	"runtime/debug"
+	"github.com/pefish/go-task-driver"
+	"log"
 )
 
 func main() {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println(err)
-			fmt.Println(string(debug.Stack()))
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}()
-
 	go_config.Config.MustLoadYamlConfig(go_config.Configuration{
 		ConfigEnvName: `GO_CONFIG`,
 		SecretEnvName: `GO_SECRET`,
 	})
 
-	go_core.Service.SetName(`_template_`)
+	service.Service.SetName(`_template_`)
 	// 处理日志
-	go_application.Application.SetEnv(go_config.Config.GetString(`env`))
-	go_logger.Logger = go_logger.NewLogger(go_logger.WithName(go_core.Service.GetName()), go_logger.WithIsDebug(go_application.Application.Debug))
+	go_application.Application.SetEnv(go_config.Config.MustGetString(`env`))
+	go_logger.Logger = go_logger.NewLogger(go_logger.WithIsDebug(go_application.Application.Debug))
 	logger.LoggerDriver.Register(go_logger.Logger)
 
 	// 初始化数据库连接
-	mysqlConfig := go_config.Config.MustGetMap(`mysql`)
+	mysqlConfig, err := go_config.Config.GetMap(`mysql`)
+	if err != nil {
+		log.Fatal(err)
+	}
 	go_mysql.MysqlHelper.SetLogger(go_logger.Logger)
 	go_mysql.MysqlHelper.MustConnectWithMap(mysqlConfig)
 	defer go_mysql.MysqlHelper.Close()
 
-	go_core.Service.SetHost(go_config.Config.GetString(`host`))
-	go_core.Service.SetPort(go_config.Config.GetUint64(`port`))
-	go_core.Service.SetPath(`/api`)
+	service.Service.SetHost(go_config.Config.MustGetString(`host`))
+	service.Service.SetPort(go_config.Config.MustGetUint64Default(`port`, 8000))
+	service.Service.SetPath(`/api`)
 	global_api_strategy.ParamValidateStrategy.SetErrorCode(constant.PARAM_ERROR)
 
-	go_core.Service.SetRoutes(route.LoginRoute)
-	go_core.Service.Run()
+	service.Service.SetRoutes(route.LoginRoute)
+
+	taskDriver := task_driver.NewTaskDriver()
+	taskDriver.SetLogger(go_logger.Logger)
+	taskDriver.Register(service.Service.GetName(), service.Service)
+
+	taskDriver.RunWait()
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module _template_
 go 1.14
 
 require (
-	github.com/pefish/go-application v0.1.3
-	github.com/pefish/go-config v0.1.0
-	github.com/pefish/go-core v1.0.3
-	github.com/pefish/go-logger v0.2.3
-	github.com/pefish/go-mysql v0.7.7
+	github.com/pefish/go-application v0.1.4
+	github.com/pefish/go-config v0.1.11
+	github.com/pefish/go-core v1.0.5
+	github.com/pefish/go-logger v0.2.9
+	github.com/pefish/go-mysql v0.8.4
+	github.com/pefish/go-task-driver v0.0.4
 )
