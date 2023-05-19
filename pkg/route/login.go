a71d889068a72824f070900417d997569dfa747a
pefish <darrondarron@qq.com>
2023-05-19 11:13:55 +0800

    add

--- pkg/route/login.go
+++ pkg/route/login.go
@@ -3,13 +3,14 @@ package route
 import (
 	"_template_/pkg/controller"
 	"github.com/pefish/go-core/api"
+	"github.com/pefish/go-http/gorequest"
 )
 
 var LoginRoute = []*api.Api{
 	{
 		Description: "登录",
 		Path:        "/v1/login",
-		Method:      "POST",
+		Method:      gorequest.POST,
 		Params: controller.LoginParam{
 			Username: `test`,
 		},
