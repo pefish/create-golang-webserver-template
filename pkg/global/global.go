e0627f71c9650c0b79192c7d1f3e8b3d6abbc2f5
pefish <darrondarron@qq.com>
2023-12-20 16:10:52 +0800

    add

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -3,6 +3,13 @@ package global
 type Config struct {
 	Host string `json:"host"`
 	Port uint64 `json:"port"`
+	Db   struct {
+		Db       string `json:"db"`
+		Host     string `json:"host"`
+		ReadHost string `json:"readHost"`
+		User     string `json:"user"`
+		Pass     string `json:"pass"`
+	} `json:"db"`
 }
 
 var GlobalConfig Config
