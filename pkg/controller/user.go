7a591ab2ec6dae54f54da0f1a651253d077591d1
pefish <darrondarron@qq.com>
2023-12-27 18:05:19 +0800

    add

--- pkg/controller/user.go
+++ pkg/controller/user.go
@@ -3,6 +3,7 @@ package controller
 import (
 	_type "github.com/pefish/go-core-type/api-session"
 	go_error "github.com/pefish/go-error"
+	go_logger "github.com/pefish/go-logger"
 )
 
 type UserControllerType struct {
@@ -17,7 +18,11 @@ type LoginParams struct {
 
 func (lc *UserControllerType) Login(apiSession _type.IApiSession) (interface{}, *go_error.ErrorInfo) {
 	var params LoginParams
-	apiSession.MustScanParams(&params)
+	err := apiSession.ScanParams(&params)
+	if err != nil {
+		go_logger.Logger.ErrorF("Read params error. %+v", err)
+		return nil, go_error.INTERNAL_ERROR
+	}
 
 	return params, nil
 }
