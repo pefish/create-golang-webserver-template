ec860387eab79442f0b08e708f3f34b936780052
pefish <darrondarron@qq.com>
2024-04-24 18:01:43 +0800

    add

--- cmd/app-name/main.go
+++ cmd/app-name/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"github.com/pefish/go-commander"
-	"github.com/pefish/go-logger"
+	"fmt"
 	"package-name/cmd/app-name/command"
 	"package-name/version"
+
+	"github.com/pefish/go-commander"
+	go_logger "github.com/pefish/go-logger"
 )
 
 func main() {
-	commanderInstance := commander.NewCommander(version.AppName, version.Version, version.AppName+" 是一个模版，祝你玩得开心。作者：pefish")
+	commanderInstance := commander.NewCommander(
+		version.AppName,
+		version.Version,
+		fmt.Sprintf("%s is a template. Author: pefish", version.AppName),
+	)
 	commanderInstance.RegisterDefaultSubcommand(&commander.SubcommandInfo{
-		Desc:       "",
+		Desc:       "Use this command by default if you don't set subcommand.",
 		Args:       nil,
 		Subcommand: command.NewDefaultCommand(),
 	})
